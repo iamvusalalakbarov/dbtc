574d7f81375ce3e8c4398c867674587c761adc86
Vusal Alakbarov <vusal11010@gmail.com>
2023-06-08 21:11:37 +0400

    Implemented Log In logic

--- view/log-in.php
+++ view/log-in.php
@@ -1,3 +1,32 @@
+<?php
+
+if (isset($_POST["submit"])) {
+  $username = $_POST["username"];
+  $password = $_POST["password"];
+
+  $users = $db->query("SELECT username, password FROM users")->fetchAll(PDO::FETCH_ASSOC);
+
+  $isMatched = false;
+
+  foreach ($users as $user) {
+    if ($user["username"] == $username && password_verify($password, $user["password"])) {
+      $isMatched = true;
+      break;
+    }
+  }
+
+  if ($isMatched) {
+    $_SESSION["loggedIn"] = true;
+    $_SESSION["username"] = $username;
+
+    header("Location:home");
+  } else {
+    echo "<script>alert('Username and password does not matched.')</script>;";
+  }
+}
+
+?>
+
 <main class="log-in">
   <h3>Log In</h3>
 
