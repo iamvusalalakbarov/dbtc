ab7fdaa27e01537bd84c317f424faa3aef7c17db
Vusal Alakbarov <vusal11010@gmail.com>
2023-06-13 22:44:22 +0400

    Rendered active chains under a form

--- view/home.php
+++ view/home.php
@@ -6,9 +6,9 @@ if (isset($_SESSION["loggedIn"]) && isset($_SESSION["username"])) {
     $user = $query->fetch(PDO::FETCH_ASSOC);
     $userID = $user["userID"];
 
-    $query = $db->prepare("SELECT * FROM chains WHERE userID = ?");
+    $query = $db->prepare("SELECT * FROM chains WHERE userID = ? AND isActive = 1");
     $insert = $query->execute([$userID]);
-    $chains = $query->fetchAll(PDO::FETCH_ASSOC);
+    $activeChains = $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
 ?>
@@ -26,7 +26,7 @@ if (isset($_SESSION["loggedIn"]) && isset($_SESSION["username"])) {
             </p>
         </div>
 
-        <a href="#">Get Started</a>
+        <a href="sign-up">Get Started</a>
     </main>
 
 <?php else : ?>
@@ -34,46 +34,63 @@ if (isset($_SESSION["loggedIn"]) && isset($_SESSION["username"])) {
     <main class="chains">
         <h1><?php echo $_SESSION["username"]; ?>'s Chains</h1>
 
-        <?php if (count($chains) > 0) : ?>
-            <table>
-                <tr>
-                    <td></td>
-                    <?php
-                    $date = new DateTime();
-                    $date->sub(new DateInterval("P10D"));
-                    ?>
-                    <?php for ($i = 10; $i > 0; $i--) : ?>
-                        <td>
-                            <?php $date->add(new DateInterval("P1D")); ?>
-                            <div class="month"><?php echo $date->format("F"); ?></div>
-                            <div class="day"><?php echo $date->format("d"); ?></div>
-                        </td>
-                    <?php endfor; ?>
-                </tr>
-                <?php foreach ($chains as $chain) : ?>
+        <?php if (count($activeChains) > 0) : ?>
+            <form method="POST">
+                <table>
                     <tr>
-                        <td><?php echo $chain["chainName"]; ?></td>
+                        <td>Chain Name</td>
                         <?php
                         $date = new DateTime();
                         $date->sub(new DateInterval("P10D"));
-                        $chainDate = new DateTime($chain["startDate"]);
-                        $chainDate->add(new DateInterval("P{$chain["length"]}D"));
                         ?>
-                        <?php for ($i = 9; $i > 0; $i--) : ?>
+                        <?php for ($i = 10; $i > 0; $i--) : ?>
                             <td>
                                 <?php $date->add(new DateInterval("P1D")); ?>
-                                <span <?php echo ($chainDate >= $date) ? "class='crossed'" : ""; ?>></span>
+                                <div class="month"><?php echo $date->format("F"); ?></div>
+                                <div class="day"><?php echo $date->format("d"); ?></div>
                             </td>
                         <?php endfor; ?>
-                        <td>
-                            <?php $date->add(new DateInterval("P1D")); ?>
-                            <input type="checkbox" name=<?php echo $chain["chainName"]; ?>>
-                            <span <?php echo ($chainDate >= $date) ? "class='crossed'" : ""; ?>></span>
-                        </td>
+                        <td>Length</td>
                     </tr>
-                <?php endforeach; ?>
-            </table>
+                    <?php foreach ($activeChains as $chain) : ?>
+                        <tr>
+                            <td><?php echo $chain["chainName"]; ?></td>
+                            <?php
+                            $date = new DateTime();
+                            $date->sub(new DateInterval("P10D"));
+                            $chainDate = new DateTime($chain["startDate"]);
+                            $chainDate->add(new DateInterval("P{$chain["length"]}D"));
+                            ?>
+                            <?php for ($i = 9; $i > 0; $i--) : ?>
+                                <td>
+                                    <?php $date->add(new DateInterval("P1D")); ?>
+                                    <span <?php echo ($chainDate >= $date) ? "class='crossed'" : ""; ?>></span>
+                                </td>
+                            <?php endfor; ?>
+                            <td>
+                                <?php $date->add(new DateInterval("P1D")); ?>
+                                <label <?php echo ($chainDate >= $date) ? "class='crossed'" : ""; ?>>
+                                    <input type="checkbox" name="">
+                                </label>
+                            </td>
+                            <td><?php echo $chain["length"]; ?></td>
+                            <td>
+                                <form method="POST">
+                                    <input type="hidden" name="delete" value="<?php echo $chain["chainID"]; ?>">
+                                    <button type="submit">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+                <input type="hidden" name="submit" value="1">
+                <button type="submit">Submit Changes</button>
+            </form>
+        <?php else : ?>
+            <p>You don't have any active chains yet. Create new by clicking the link below.</p>
         <?php endif; ?>
+
+        <a href="create-chain">Create Chain</a>
     </main>
 
 <?php endif; ?>
